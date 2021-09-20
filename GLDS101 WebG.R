967d83844768dd3ff5140c479674ac0d8f88aa07
ivankoros <61260021+ivankoros@users.noreply.github.com>
2021-09-20 10:47:05 -0400

    Update GLDS101 WebG.R

--- GLDS101 WebG.R
+++ GLDS101 WebG.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(WebGestaltR)
 
-GLDS101 <- read_csv("GLDS-101_rna_seq_differential_expression.csv")
+
+GLDS101 <- read_xlsx("GLDS-101_rna_seq_differential_expression.xlsx")
 
 GLDS101_Sorted <- GLDS101 %>%
   tibble() %>%
@@ -9,22 +10,29 @@ GLDS101_Sorted <- GLDS101 %>%
   filter(abs(`Log2fc_(FLT)v(GC)`) >= .305) %>%
   drop_na(SYMBOL) %>%
   filter(!grepl("RIKEN", GENENAME)) %>%
-  select(1:7, `Log2fc_(FLT)v(GC)`, `Adj.p.value_(FLT)v(GC)`)
+  select(1:5, `Log2fc_(FLT)v(GC)`, `Adj.p.value_(FLT)v(GC)`)
 
-Ensemble <- GLDS101_Sorted$ENSEMBL %>%
-  as.data.frame()
+Symbol <- GLDS101_Sorted$SYMBOL %>%
+  as.list()
+
+WebGTxt <- read.table("Webg920.txt")
 
 WebGestaltR(
   enrichMethod = "ORA",
   organism = "mmusculus",
   enrichDatabase =  'geneontology_Biological_Process',
-  enrichDatabaseType = "ensembl_gene_id",
-  interestGeneFile = "GTxt.txt",
-  interestGeneType = "ensembl_gene_id",
-  collapseMethod = "mean",
+  enrichDatabaseType = "genesymbol",
+  interestGeneFile = "Webg920.txt",
+  interestGeneType = "genesymbol",
   referenceSet = "genome",
   isOutput = TRUE,
+  fdrThr = 0.05,
+  minNum = 5,
+  maxNum = 2000,
+  sigMethod = "fdr",
+  fdrMethod = "BH",
   outputDirectory = getwd(),
+  dagColor = "continuous",
   projectName = NULL,
   gseaPlotFormat = c("png", "svg"),
   setCoverNum = 10,
